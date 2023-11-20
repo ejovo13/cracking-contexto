968c64868f4ceaa69941bebadde2d2f596e230a1
archstation <ejovo13@yahoo.com>
2023-11-20 02:17:03 +0100

    add winning words and cleanup validation script

--- check_completeness.py
+++ check_completeness.py
@@ -5,27 +5,29 @@ from contextocracker.data_collection_async import (
     get_words,
     make_api_requests,
 )
-from contextocracker.resources import brown_and_alpha_filtered
+from contextocracker.resources import brown_and_alpha_filtered, winning_words
 
-all_words = set(get_words(brown_and_alpha_filtered()))
+def retrieve_winning_words(game_id: int) -> list[str]:
+    """Retrieve a list of winning words up until game number `game_id`."""
+    give_up_url = lambda game_id: f"https://api.contexto.me/machado/en/giveup/{game_id}"
 
-give_up_url = lambda game_id: f"https://api.contexto.me/machado/en/giveup/{game_id}"
+    # Get a list of all the winners up until 425
+    urls = [give_up_url(idx) for idx in range(0, game_id)]
+    responses = make_api_requests(urls, chunk_size=50)
+    winning_ranks = [extract_word_rank(response) for response in responses]
+    return [w for w, _ in winning_ranks]
 
-# Get a list of all the winners up until 425
-urls = [give_up_url(idx) for idx in range(0, 426)]
-responses = make_api_requests(urls, chunk_size=50)
+def missing_words(word_list_path: str = brown_and_alpha_filtered()) -> list[str]:
+    """Return a list of winning words that don't appear in brown_and_alpha_filtered."""
 
-missing_words = []
+    all_words = set(get_words(word_list_path))
+    winners = winning_words()
+    return [lemma for lemma in winners if lemma not in all_words]
 
-for response in responses:
-    try:
-        lemma, rank = extract_word_rank(response)
-        # print("winning word: {}".format(lemma))
+if __name__ == '__main__':
+    print(missing_words())
+    print(missing_words("top_3000.txt"))
 
-        if lemma not in all_words:
-            missing_words.append(lemma)
+    print(len(missing_words("top_3000.txt")))
 
-    except:
-        print("Something went wrong!")
 
-print(missing_words)
